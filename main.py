415a038a9102b30e5cc50b8d7a6edeb1d78153ca
codingservices2020 <codingservices2020@gmail.com>
2025-02-22 12:40:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from telegram.ext import (
 )
 from telegram.error import BadRequest
 from datetime import datetime, timedelta
+
 from apscheduler.schedulers.background import BackgroundScheduler
 import asyncio
 import logging
@@ -20,11 +21,11 @@ import string
 import time
 import os
 
-from keep_alive import keep_alive
-keep_alive()
+# from keep_alive import keep_alive
+# keep_alive()
 
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 # Configure logging
 logging.basicConfig(
@@ -46,19 +47,6 @@ ADMIN_CHAT_ID = int(os.getenv('ADMIN_CHAT_ID'))
 subscription_data = {}
 user_data = {}
 codes_data = {}
-SUBSCRIPTION_FILE = {
-  "type": "service_account",
-  "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-  "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-  "private_key": os.getenv("FIREBASE_PRIVATE_KEY", "").replace('\\n', '\n'),  # Convert \n into real newlines
-  "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-  "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-  "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-  "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-  "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-  "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
-  "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
-}
 CODES_FILE = "codes.json"
 price = 1500
 
